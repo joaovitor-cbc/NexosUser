155a2e60b32f4225547b612182b90b63d2955ac3
João Vitor <joaovitor.novacruz@gmail.com>
2023-01-22 18:45:28 -0300

    Config e ajuste de estrutura do projeto

---
+++ app/src/main/java/com/nexos/user/NexosUserApplication.java
@@ -0,0 +1,13 @@
+package com.nexos.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexosUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexosUserApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/nexos/user/NexosUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexos.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexosUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
